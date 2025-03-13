0408e35ad321186cf5c5038041ee7a4c43ce77c8
SergWebAcceptance <serg.web@acceptance.ltd>
2025-03-13 10:12:50 +0200

    fixed orders

--- src/collections/Orders.ts
+++ src/collections/Orders.ts
@@ -151,9 +151,13 @@ export const Orders: CollectionConfig = {
     ],
     afterChange: [
       async ({ doc, req }) => {
-        console.log('doc', doc)
-        if (doc.status === 'completed' && doc.files) {
-          let recipientEmail: string | undefined
+        if (
+          doc.status === 'completed' &&
+          doc.items &&
+          Array.isArray(doc.items) &&
+          doc.items.length > 0
+        ) {
+          let recipientEmail
           if (doc.user) {
             const user = await req.payload.findByID({
               collection: 'users',
@@ -165,133 +169,94 @@ export const Orders: CollectionConfig = {
             recipientEmail = process.env.DEFAULT_ORDER_NOTIFICATION_EMAIL
           }
 
-          const fileDoc = await req.payload.findByID({
-            collection: 'media',
-            id: doc.files,
-          })
-          const fileUrl = fileDoc?.url || '#'
+          const downloadButtonsHtml = doc.items
+            .filter((item: { file_url?: string }) => item.file_url)
+            .map(
+              (item: { file_url?: string; file_name?: string }) => `
+                <a href="${item.file_url}" 
+                  style="color: #0C0B0E;
+                          width:100%;
+                         font-size: 16px;
+                         font-style: normal;
+                         font-weight: 500;
+                         line-height: normal;
+                         padding: 16px 24px;
+                         border-radius: 16px;
+                         background: #FFF;
+                         display: block;
+                         text-decoration: none;
+                         text-align: center;
+                         margin-bottom: 10px;">
+                  Download ${item.file_name || 'File'}
+                </a>
+              `,
+            )
+            .join('')
 
           const htmlContent = `<table width="640"
-    style="border-collapse: collapse; margin: 0 auto;  font-family: Roboto, sans-serif;border: none;background: #141316;">
-    <thead style="border: none;">
-        <tr style="border: none;">
-            <td style="border: none;">
-                <img style="width: 100%;display: block;" src="https://modulixo.com/images/email_header.png"
-                    alt="Header" />
-            </td>
-        </tr>
-    </thead>
-    <tbody style="border: none;">
-        <tr style="border: none;">
-            <td style="padding: 40px 40px 0 40px;background: #141316;border: none;">
-                <h2 style="color: #FFF;
-                font-size: 24px;
-                font-style: normal;
-                font-weight: 700;
-                line-height: normal;
-                margin-bottom: 40px;">Your Modulixo Order is Ready! - #${doc.orderNumber}</h2>
-                <p style="color: #808080;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: normal;">Dear customer,</p>
-                <p style="color: #808080;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: normal;">
-                    Thank you for your purchase from Modulixo! Your order has been successfully processed, and your
-                    files are now ready for download.</p>
-                <h3 style="color: #FFF;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 700;
-                line-height: normal;
-                margin: 40px 0 20px 0;">
+            style="border-collapse: collapse; margin: 0 auto; font-family: Roboto, sans-serif; border: none; background: #141316;">
+            <thead style="border: none;">
+              <tr style="border: none;">
+                <td style="border: none;">
+                  <img style="width: 100%; display: block;" src="https://modulixo.com/images/email_header.png" alt="Header" />
+                </td>
+              </tr>
+            </thead>
+            <tbody style="border: none;">
+              <tr style="border: none;">
+                <td style="padding: 40px 40px 0 40px; background: #141316; border: none;">
+                  <h2 style="color: #FFF; font-size: 24px; font-weight: 700; margin-bottom: 40px;">
+                    Your Modulixo Order is Ready! - #${doc.orderNumber}
+                  </h2>
+                  <p style="color: #808080; font-size: 16px; font-weight: 400;">
+                    Dear customer,
+                  </p>
+                  <p style="color: #808080; font-size: 16px; font-weight: 400;">
+                    Thank you for your purchase from Modulixo! Your order has been successfully processed, and your files are now ready for download.
+                  </p>
+                  <h3 style="color: #FFF; font-size: 16px; font-weight: 700; margin: 40px 0 20px 0;">
                     Download Your Files Here:
-                </h3>
-                <a href="https://cms.modulixo.com${fileUrl}" style="color: #0C0B0E;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 500;
-                line-height: normal;
-                padding: 16px 24px;
-                border-radius: 16px;
-                background: #FFF;
-                display: block;
-                text-decoration: none;
-                width: 150px;
-                text-align: center;">
-                    Download
-                </a>
-            </td>
-        </tr>
-        <tr style="border: none;">
-            <td style="padding: 40px 40px 40px 40px;background: #141316;border: none;">
-
-                <p style="color: #808080;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 400;
-                line-height: normal;">
-                    If you have trouble accessing your files or need further assistance, don't hesitate to contact us -
-                    we're here to help!<br><br>
-
-                    We appreciate your trust in Modulixo and look forward to working with you again.</p>
-                <h3 style="color: #FFF;
-                font-size: 16px;
-                font-style: normal;
-                font-weight: 700;
-                line-height: normal;
-                margin: 40px 0 20px 0;">
+                  </h3>
+                  ${downloadButtonsHtml}
+                </td>
+              </tr>
+              <tr style="border: none;">
+                <td style="padding: 40px; background: #141316; border: none;">
+                  <p style="color: #808080; font-size: 16px; font-weight: 400;">
+                    If you have trouble accessing your files or need further assistance, don't hesitate to contact us - we're here to help!
+                    <br><br>
+                    We appreciate your trust in Modulixo and look forward to working with you again.
+                  </p>
+                  <h3 style="color: #FFF; font-size: 16px; font-weight: 700; margin: 40px 0 20px 0;">
                     Best regards,<br>
                     The Modulixo Team
-                </h3>
-            </td>
-        </tr>
-    </tbody>
-    <tfoot
-        style="background-color: #0B0B0E; background-image: url(https://modulixo.com/images/email_footer.png);background-position: center right; background-size: cover;">
-        <tr>
-            <td style="padding: 50px 40px;">
-                <table>
+                  </h3>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot style="background-color: #0B0B0E; background-image: url(https://modulixo.com/images/email_footer.png); background-position: center right; background-size: cover;">
+              <tr>
+                <td style="padding: 50px 40px;">
+                  <table>
                     <tr>
-                        <td style="width: 100px; padding: 0 8px;vertical-align: baseline;">
-                            <p style="color: #808080;
-                            font-size: 12px;
-                            font-style: normal;
-                            font-weight: 400;
-                            line-height: normal;">Phone</p>
-                            <p style="margin: 0;">
-                                <a href="tel: +48573589252" style="color: #FFF;
-                                font-size: 12px;
-                                font-style: normal;
-                                font-weight: 400;
-                                line-height: normal;"> +48573589252</a>
-                            </p>
-                        </td>
-                        <td style="width: 100px; padding: 0 8px;vertical-align: baseline;">
-                            <p style="color: #808080;
-                            font-size: 12px;
-                            font-style: normal;
-                            font-weight: 400;
-                            line-height: normal;">Email</p>
-                            <p style="margin: 0;">
-                                <a href="mailto:info@modulixo.com" style="color: #FFF;
-                                font-size: 12px;
-                                font-style: normal;
-                                font-weight: 400;
-                                line-height: normal;">info@modulixo.com</a>
-                            </p>
-                        </td>
-
-
+                      <td style="width: 100px; padding: 0 8px;">
+                        <p style="color: #808080; font-size: 12px;">Phone</p>
+                        <p style="margin: 0;">
+                          <a href="tel:+48573589252" style="color: #FFF; font-size: 12px;">+48573589252</a>
+                        </p>
+                      </td>
+                      <td style="width: 100px; padding: 0 8px;">
+                        <p style="color: #808080; font-size: 12px;">Email</p>
+                        <p style="margin: 0;">
+                          <a href="mailto:info@modulixo.com" style="color: #FFF; font-size: 12px;">info@modulixo.com</a>
+                        </p>
+                      </td>
                     </tr>
-                </table>
-            </td>
-        </tr>
-    </tfoot>
-</table>`
+                  </table>
+                </td>
+              </tr>
+            </tfoot>
+          </table>`
 
           try {
             await req.payload.sendEmail({
